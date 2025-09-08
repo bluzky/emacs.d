e710d14e3a379b9932490553c0d7020a7f8eddfb
Always blue <bluesky.1289@gmail.com>
2025-09-08 14:57:45 +0700

    Update theme

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -46,7 +46,7 @@
     "Load theme, taking current system APPEARANCE into consideration."
     (mapc #'disable-theme custom-enabled-themes)
     (pcase appearance
-      ('light (load-theme 'ef-day t))
+      ('light (load-theme 'ef-cyprus t))
       ('dark (load-theme 'ef-owl t))))
 
   (add-hook 'ns-system-appearance-change-functions #'me/apply-theme))
